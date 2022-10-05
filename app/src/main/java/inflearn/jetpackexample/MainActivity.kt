7784fb68e274e551182f68f1695273da4ae731fa
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-10-05 20:12:49 +0900

    findViewById / Kotlin Extensions
    구글에서 Kotlin Extensions 사용을 권장하지 않는 이유

--- app/src/main/java/inflearn/jetpackexample/MainActivity.kt
+++ app/src/main/java/inflearn/jetpackexample/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_second.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,5 +25,12 @@ class MainActivity : AppCompatActivity() {
         btn1.text = "new text1"
         btn2.text = "new text2"
         btn3.text = "new text3"
+
+        //NullPointerException 발생 -> 앱 중단됨
+        second_btn1.text = "new text1"
+        second_btn2.text = "new text2"
+        second_btn3.text = "new text3"
+
+        //구글에서는 ViewBinding, DataBinding 사용 권장
     }
 }
